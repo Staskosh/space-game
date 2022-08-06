bf3f937d9dfcc2d3deb6b7a0cae766fb9853f973
staskosh <staskoshenkov@rambler.ru>
2022-08-06 20:03:30 +0300

    Add the star

--- space-game.py
+++ space-game.py
@@ -4,10 +4,20 @@ import curses
 
 def draw(canvas):
     row, column = (5, 20)
-    canvas.addstr(row, column, 'Hello, World!')
-    canvas.border()
-    canvas.refresh()
-    time.sleep(3)
+    while True:
+        canvas.border()
+        canvas.addstr(row, column, '*', curses.A_DIM)
+        canvas.refresh()
+        time.sleep(2)
+        canvas.refresh()
+        canvas.addstr(row, column, '*')
+        time.sleep(0.3)
+        canvas.refresh()
+        canvas.addstr(row, column, '*', curses.A_BOLD)
+        canvas.refresh()
+        time.sleep(0.5)
+        canvas.addstr(row, column, '*')
+        time.sleep(0.3)
 
 
 if __name__ == '__main__':
