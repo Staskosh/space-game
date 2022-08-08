5a3088e0b6599721577e30df04fe4631d1d4dbe9
staskosh <staskoshenkov@rambler.ru>
2022-08-08 10:48:32 +0300

    Add the star via the async function.

--- space-game.py
+++ space-game.py
@@ -1,23 +1,46 @@
+import asyncio
 import time
 import curses
 
 
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        canvas.refresh()
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        canvas.refresh()
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        canvas.refresh()
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        canvas.refresh()
+        await asyncio.sleep(0)
+
+
 def draw(canvas):
     row, column = (5, 20)
+    coroutine = blink(canvas, row, column, symbol='*')
     while True:
-        canvas.border()
-        canvas.addstr(row, column, '*', curses.A_DIM)
-        canvas.refresh()
-        time.sleep(2)
-        canvas.refresh()
-        canvas.addstr(row, column, '*')
-        time.sleep(0.3)
-        canvas.refresh()
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        canvas.refresh()
-        time.sleep(0.5)
-        canvas.addstr(row, column, '*')
-        time.sleep(0.3)
+        curses.curs_set(False)
+        coroutine.send(None)
+        # canvas.border()
+        # canvas.addstr(row, column, '*', curses.A_DIM)
+        # canvas.refresh()
+        # time.sleep(2)
+        # canvas.refresh()
+        # canvas.addstr(row, column, '*')
+        # time.sleep(0.3)
+        # canvas.refresh()
+        # canvas.addstr(row, column, '*', curses.A_BOLD)
+        # canvas.refresh()
+        # time.sleep(0.5)
+        # canvas.addstr(row, column, '*')
+        # time.sleep(0.3)
 
 
 if __name__ == '__main__':
