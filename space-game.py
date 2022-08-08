13429317dcbf2fb43ce32313cfd72d035c41a976
staskosh <staskoshenkov@rambler.ru>
2022-08-08 11:01:03 +0300

    Add 5 stars

--- space-game.py
+++ space-game.py
@@ -6,28 +6,38 @@ import curses
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        canvas.refresh()
         await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        canvas.refresh()
         await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        canvas.refresh()
         await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        canvas.refresh()
         await asyncio.sleep(0)
 
 
 def draw(canvas):
     row, column = (5, 20)
-    coroutine = blink(canvas, row, column, symbol='*')
+    first_coroutine = blink(canvas, row, column, symbol='*')
+    second_coroutine = blink(canvas, row=5, column=10, symbol='*')
+    third_coroutine = blink(canvas, row=5, column=15, symbol='*')
+    fourth_coroutine = blink(canvas, row=5, column=5, symbol='*')
+    fifth_coroutine = blink(canvas, row=5, column=0, symbol='*')
+    coroutines = [
+        first_coroutine,
+        second_coroutine,
+        third_coroutine,
+        fourth_coroutine,
+        fifth_coroutine
+    ]
     while True:
-        curses.curs_set(False)
-        coroutine.send(None)
+        for coroutine in coroutines.copy():
+            curses.curs_set(False)
+            coroutine.send(None)
+            canvas.refresh()
+        time.sleep(1)
         # canvas.border()
         # canvas.addstr(row, column, '*', curses.A_DIM)
         # canvas.refresh()
