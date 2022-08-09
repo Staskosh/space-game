03583d24319a886a4e625bafe6c64fa7cc7d3e96
staskosh <staskoshenkov@rambler.ru>
2022-08-09 14:33:11 +0300

    Add a spaceship.

--- space-game.py
+++ space-game.py
@@ -1,9 +1,25 @@
 import asyncio
+import os
 import random
 import time
 import curses
+from itertools import cycle
+
+from dotenv import load_dotenv
+
+from curses_tools import draw_frame
 
 TIC_TIMEOUT = 0.1
+
+
+async def animate_spaceship(canvas, animations, row, column):
+    for frame in cycle(animations):
+        draw_frame(canvas, row, column, frame)
+        canvas.refresh()
+        time.sleep(0.3)
+        await asyncio.sleep(0)
+
+        draw_frame(canvas, row, column, frame, negative=True)
 
 
 async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
@@ -65,13 +81,26 @@ def create_stars_parameters(signs, max_y, max_x, stars_number):
     return star_params
 
 
+def get_animations():
+    animations_directory = os.getenv('ANIMATIONS_FOLDER')
+    animations = []
+    for file in os.listdir(animations_directory):
+        with open(f'{animations_directory}/{file}', 'r', encoding='KOI8-R') as my_file:
+            animation = my_file.read()
+        animations.append(animation)
+
+    return animations
+
+
 def draw(canvas):
     stars_number = 50
     signs = ['+', '*', '.', ':']
+    animations = get_animations()
     max_y, max_x = canvas.getmaxyx()
     star_params = create_stars_parameters(signs, max_y, max_x, stars_number)
     coroutines = [blink(canvas, row, column, symbol) for row, column, symbol in star_params]
-    coroutines.append(fire(canvas, start_row=max_y//2, start_column=max_x//2, rows_speed=-0.3, columns_speed=0))
+    # coroutines.append(fire(canvas, start_row=max_y//2, start_column=max_x//2, rows_speed=-0.3, columns_speed=0))
+    coroutines.append(animate_spaceship(canvas, animations, row=max_y//3, column=max_x//2))
     while True:
         try:
             for coroutine in coroutines.copy():
@@ -79,7 +108,7 @@ def draw(canvas):
                 coroutine.send(None)
                 canvas.refresh()
                 canvas.border()
-            time.sleep(0.1)
+            time.sleep(TIC_TIMEOUT)
         except StopIteration:
             coroutines.remove(coroutine)
         if len(coroutines) == 0:
@@ -87,9 +116,10 @@ def draw(canvas):
 
 
 def main():
+    load_dotenv()
     curses.update_lines_cols()
     curses.wrapper(draw)
-
+    curses.wrapper(animate_spaceship)
 
 
 if __name__ == '__main__':
